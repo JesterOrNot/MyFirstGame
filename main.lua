c0efe1db014b6bb7fc9cc64b8e9a87e040c5a6b1
Sean Hellum <seanhellum45@gmail.com>
2020-01-04 19:58:46 +0000

    Initialize Board

--- main.lua
+++ main.lua
@@ -1,4 +1,8 @@
 function love.draw()
-    love.graphics.setBackgroundColor(64 / 255, 160 / 255, 107 / 255)
-    love.graphics.print("Hello World From Love2D!", 99, 290, 0, 4)
+    love.graphics.setBackgroundColor(5, 5, 5)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.line(0, 190, 1000, 190)
+    love.graphics.line(0, 390, 1000, 390)
+    love.graphics.line(250, 0, 250, 1000)
+    love.graphics.line(550, 0, 550, 1000)
 end
