c6ccd5878d462d9a8efb5deceee686c953ece2be
Sean Hellum <seanhellum45@gmail.com>
2020-01-04 19:42:50 +0000

    update sizing

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
 function love.draw()
     love.graphics.setBackgroundColor(64 / 255, 160 / 255, 107 / 255)
-    love.graphics.print("Hello World From Love2D", 400, 300)
+    love.graphics.print("Hello World From Love2D!", 99, 290, 0, 4)
 end
