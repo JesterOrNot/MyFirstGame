1f93c41bfa4ecb07ecca9d00cfd95b90f6312d2d
Sean Hellum <seanhellum45@gmail.com>
2020-01-04 20:11:25 +0000

    update line positioning

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 function love.draw()
     love.graphics.setBackgroundColor(5, 5, 5)
     love.graphics.setColor(0, 0, 0)
-    love.graphics.line(0, 190, 1000, 190)
-    love.graphics.line(0, 390, 1000, 390)
-    love.graphics.line(250, 0, 250, 1000)
-    love.graphics.line(550, 0, 550, 1000)
+    love.graphics.line(80, 190, 600, 190)
+    love.graphics.line(80, 390, 600, 390)
+    love.graphics.line(250, 50, 250, 500)
+    love.graphics.line(450, 50, 450, 500)
 end
