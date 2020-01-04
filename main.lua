c7243345f4951a8cb50b438f44e14882e52225dc
Sean Hellum <seanhellum45@gmail.com>
2020-01-04 19:35:23 +0000

    change message

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
 function love.draw()
     love.graphics.setBackgroundColor(64 / 255, 160 / 255, 107 / 255)
-    love.graphics.print("Hello World", 400, 300)
+    love.graphics.print("Hello World From Love2D", 400, 300)
 end
